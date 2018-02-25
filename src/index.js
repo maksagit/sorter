d91114a70cc76c6ce06528bc0466ca4e7dcd5b75
maksagit <maksibon@gmail.com>
2018-02-25 18:58:53 +0300

    Sorting solution

--- src/index.js
+++ src/index.js
@@ -1,30 +1,63 @@
 class Sorter {
   constructor() {
-    // your implementation
+    // creates instance of Sorter class
+  this.array = [];
+  
+  this.sorting = function (x,y)
+  {
+    if (x > y)      // 'x' is greater than 'y' by the ordering criterion
+      return 1;
+    else if (x < y) // 'x' is less than 'y' by some ordering criterion
+      return -1;
+    // 'x' must be equal to 'y'
+    else return 0;
   }
-
+}
   add(element) {
-    // your implementation
+    //add element and store it in any way inside
+    this.array.push(element);
+    return this.array;
   }
 
   at(index) {
-    // your implementation
+    return this.array[index];
   }
 
   get length() {
-    // your implementation
+    //return the count of current elements, which were added to Sorter instance via add method
+    return this.array.length;
   }
 
   toArray() {
-    // your implementation
+    //return all elements in array  
+    return this.array;
   }
 
   sort(indices) {
-    // your implementation
+    //takes indices of already added elements and sorts only these elements
+    indices.sort(function(x, y) {
+      return x - y;
+    });
+
+    var indArray = [];
+
+    for (let i=0; i<indices.length;i++) { 
+      indArray.push(this.array[indices[i]]); 
+    }
+	  
+	  indArray.sort(this.sorting);
+	  
+	  for (let i=0; i<indices.length;i++) { 
+      this.array[indices[i]] = indArray[i]; 
+    }
+
+	  return this.array; 
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    /*  Takes compareFunction as parameter and use it while 
+    sorting elements. Sort works for numbers by default */
+    this.sorting = compareFunction;
   }
 }
 
